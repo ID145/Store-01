cd4cba9e07cf8fcd98f7561d718dc0f3df8f4a91
ID145 <LCZ145@163.com>
2017-10-02 10:59:00 +0800

    Delete 01.php

--- 01.php
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python27
-import socket
-c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-saddr = ('192.168.0.155', 8026)
-c.connect(saddr)  
-c.send('hello server')
-data = c.recv(1024)
-print "Reply from server %s" % data
